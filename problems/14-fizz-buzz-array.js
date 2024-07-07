ed57c952e140b9070b54c995b134a03152d0c1a2
Kourani <12mohamadkourani@gmail.com>
2024-07-07 12:17:56 -0700

    16 complete

--- problems/14-fizz-buzz-array.js
+++ problems/14-fizz-buzz-array.js
@@ -6,6 +6,20 @@ divisible by either 3 or 5, but not both.
 
 // Your code here 
 
+function fizzBuzz(max){
+
+    let array = []
+
+    for(let i=1; i<max; i++){
+        
+        if(i%3===0 || i%5===0 && not both (i%3===0 && i%5===0)){
+            array.push(i)
+        }
+    }
+
+    return array
+}
+
 
 // console.log(fizzBuzz(12)); // [ 3, 5, 6, 9, 10 ]
 // console.log(fizzBuzz(20)); // [ 3, 5, 6, 9, 10, 12, 18 ]
--- problems/16-even-numbers.js
+++ problems/16-even-numbers.js
@@ -6,6 +6,18 @@ max.
 
 // Your code here 
 
+function evenNumbers(max){
+    let array = []
+
+    for(let i=1; i<max; i++){
+        if(i%2===0){
+            array.push(i)
+        }
+    }
+
+    return array 
+}
+
 
 // console.log(evenNumbers(7));  // [ 2, 4, 6 ]
 // console.log(evenNumbers(12)); // [ 2, 4, 6, 8, 10 ]
