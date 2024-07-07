b4820ab6e3de4636fdc021e6aeb11571aa972a8f
Kourani <12mohamadkourani@gmail.com>
2024-07-07 11:16:32 -0700

    11 complete

--- problems/11-less-than-five.js
+++ problems/11-less-than-five.js
@@ -5,6 +5,17 @@ array containing all the numbers in the input array that are less than 5.
 
 // Your code here 
 
+function lessThan5(array){
+    
+    let newArray=[]
+    for(let i=0; i<array.length; i++){
+        if(array[i]<5){
+            newArray.push(array[i])
+        }
+    }
+    return newArray
+}
+
 // let nums1 = [1, -5, 10, 6, 2];
 // let filteredNums1 = lessThan5(nums1);
 // console.log(filteredNums1);           //=> [1, -5, 2]
